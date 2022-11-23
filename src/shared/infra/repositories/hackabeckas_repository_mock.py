df58397d63881264277f4c02bfb336a4fbcc0c89
Luigi Trevisan <checkzin123@gmail.com>
2022-11-23 19:43:47 -0300

    Otimização de mock (de verdade)🤓

--- src/shared/infra/repositories/hackabeckas_repository_mock.py
+++ src/shared/infra/repositories/hackabeckas_repository_mock.py
@@ -6,66 +6,43 @@ from src.shared.domain.enums.border_enum import BORDER
 from src.shared.domain.enums.flavor_enum import FLAVOR
 
 class HackabeckasRepositoryMock(IHackabeckasRepository):
-    borders = [border for border in BORDER]
+    pizzas = list[Pizza]
+    tables = list[Table]
     orders = list[Order]
     
     def __init__(self):
-      self.orders = [
-          Order(
-              pizza= Pizza(flavor=FLAVOR.MUSSARELA, border=None),
-              table= Table(tableNumber=1, numberOfPeople=2)
-                ),
-          
-          Order(
-              pizza= Pizza(flavor=FLAVOR.PEPPERONI, border=BORDER.CATUPIRY),
-              table= Table(tableNumber=218, numberOfPeople=8)
-                ),
-          
-          Order(
-              pizza= Pizza(flavor=FLAVOR.CALABRESA, border=None),
-              table= Table(tableNumber=0, numberOfPeople=2)
-                ),
-          
-          Order(
-              pizza= Pizza(flavor=FLAVOR.ATUM, border=BORDER.CHEDDAR),
-              table= Table(tableNumber=4, numberOfPeople=1)
-                ),
-          
-          Order(
-              pizza= Pizza(flavor=FLAVOR.BACON, border=BORDER.CATUPIRY),
-              table= Table(tableNumber=5, numberOfPeople=6)
-                ),
-          
-          Order(
-              pizza= Pizza(flavor=FLAVOR.RUCULA, border=BORDER.CHEDDAR),
-              table= Table(tableNumber=6, numberOfPeople=1)
-                ),
-          Order(
-              pizza= Pizza(flavor=FLAVOR.FRANGO, border=None),
-              table= Table(tableNumber=7, numberOfPeople=2)
-                ),
-          
-          Order(
-              pizza= Pizza(flavor=FLAVOR.MARGUERITA, border=BORDER.CATUPIRY),
-              table= Table(tableNumber=8, numberOfPeople=4)
-                ),
-          
-          Order(
-              pizza= Pizza(flavor=FLAVOR.PORTUGUESA, border=BORDER.CHEDDAR),
-              table= Table(tableNumber=9, numberOfPeople=10)
-                ),
-          
-          Order(
-              pizza= Pizza(flavor=FLAVOR.BRASILEIRA, border=BORDER.CATUPIRY),
-              table= Table(tableNumber=10, numberOfPeople=5)
-                ),
-          
-          Order(
-              pizza= Pizza(flavor=FLAVOR.QUATRO_QUEIJOS, border=BORDER.CHEDDAR),
-              table= Table(tableNumber=11, numberOfPeople=3)
-                )
-            
-        ]
+      
+      self.pizzas = [Pizza(
+                        flavor=FLAVOR.ATUM, border=None
+      ),
+                     Pizza(
+                       flavor=FLAVOR.MUSSARELA, border=BORDER.CATUPIRY
+                     ),
+                     Pizza(
+                       flavor=FLAVOR.CALABRESA, border=BORDER.CHEDDAR
+                     )]
+      self.tables = [Table(
+                        tableNumber=1,
+                        numberOfPeople=2
+      ),
+                     Table(
+                       tableNumber=14,
+                       numberOfPeople=4
+                     ),
+                     Table(
+                       tableNumber=12,
+                       numberOfPeople=6
+                     )]
+      self.orders = [Order(
+                      pizza=self.pizzas[0], table=self.tables[0]
+      ),
+                     Order(
+                      pizza=self.pizzas[1], table=self.tables[1]
+                     ),
+                     Order(
+                      pizza=self.pizzas[2], table=self.tables[2]
+                     )
+                     ]
         
     def create_order(self, tableNumber: int, numberOfPeople: int, flavor: FLAVOR, border: BORDER) -> Order:
       order = Order(
