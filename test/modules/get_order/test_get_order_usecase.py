10529d605c32d2310496c85387a0f382675c724a
Luigi Trevisan <checkzin123@gmail.com>
2022-12-16 18:32:13 -0300

    added orderId>0 test

--- test/modules/get_order/test_get_order_usecase.py
+++ test/modules/get_order/test_get_order_usecase.py
@@ -1,4 +1,5 @@
 import pytest
+from src.shared.helpers.errors.domain_errors import EntityError
 from src.shared.infra.repositories.hackabeckas_repository_mock import HackabeckasRepositoryMock
 from src.modules.get_order.app.get_order_usecase import GetOrderUsecase
 from src.shared.helpers.errors.usecase_errors import DuplicatedItem, NoItemsFound
@@ -8,13 +9,20 @@ class Test_GetOrderUsecase:
         repo = HackabeckasRepositoryMock()
         usecase = GetOrderUsecase(repo=repo)
         
-        user = usecase(orderId=repo.orders[1].orderId)
+        order = usecase(orderId=repo.orders[1].orderId)
         
-        assert repo.orders[1] == user
+        assert repo.orders[1] == order
         
-    def test_get_order_user_not_found(self):
+    def test_get_order_order_not_found(self):
         repo = HackabeckasRepositoryMock()
         usecase = GetOrderUsecase(repo=repo)
         
         with pytest.raises(NoItemsFound):
-            user = usecase(orderId=2004)
+            order = usecase(orderId=2004)
+    
+    def test_get_order_id_greater_than_zero(self):
+        repo = HackabeckasRepositoryMock()
+        usecase = GetOrderUsecase(repo=repo)
+        
+        with pytest.raises(EntityError):
+            order = usecase(orderId=-4)
