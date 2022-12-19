490cf712a437b1aa9130faa3be9f4589032b7869
Luigi Trevisan <checkzin123@gmail.com>
2022-12-19 15:38:39 -0300

    delete_order Viewmodel✅

---
+++ src/modules/delete_order/app/delete_order_viewmodel.py
@@ -0,0 +1,43 @@
+from src.shared.domain.entities.order import Order
+from src.shared.domain.entities.pizza import Pizza
+from src.shared.domain.entities.table import Table
+
+
+class TableViewModel:
+    table: Table
+    
+    def __init__(self, table: Table):
+        self.table = table
+        
+    def to_dict(self) -> dict:
+        return {
+            "tableNumber" : self.table.tableNumber,
+            "numberOfPeople" : self.table.numberOfPeople
+        }
+
+class PizzaViewModel:
+    pizza: Pizza
+    
+    def __init__(self, pizza: Pizza):
+        self.pizza = pizza
+    
+    def to_dict(self) -> dict:
+        return {
+            "flavor" : self.pizza.flavor.value,
+            "border" : self.pizza.border.value
+        }
+    
+
+class DeleteOrderViewmodel:
+    order: Order
+    
+    def __init__(self, order: Order):
+        self.order = order
+    
+    def to_dict(self) -> dict:
+        return {
+            "orderId" : self.order.orderId,
+            "table" : TableViewModel(self.order.table).to_dict(),
+            "pizza" : PizzaViewModel(self.order.pizza).to_dict(),
+            "message" : "The order has been deleted"
+        }
